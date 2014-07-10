5ddfc3b8aa49335584f3365dd8b7e59238a81b95
Michael <mpn123@gmail.com>
2014-07-10 16:56:22 +0200

    added threading so send and get are running concurrently.

--- cbot2.py
+++ cbot2.py
@@ -1,9 +1,13 @@
-import irc
+import irc, irc.client, threading, time
 import irc.client
+import threading
+import time
 
 client = irc.client.IRC()
 server = client.server()
 server.connect("irc.freenode.net", 6667, "cbot2")
+
+
 
 def uncrypt(encrypted_string): #un-encrypt incoming messages
 	unencrypted_string = ""
@@ -17,15 +21,28 @@ def encrypt(unencrypted_string):
 		encrypted_string += chr(ord(c) - 4)
 	return	encrypted_string
 
-def onrecieve(conneciton, evt):
+def onrecieve(_, evt):
 	print(evt.source + ": " + uncrypt(" ".join(evt.arguments)))
 
 client.add_global_handler("privmsg", onrecieve)
-client.process_once(0.1)
 
-while 1:
-	msg = input(">")
+
+def send_msg():
+	# while 1:
+	msg = input("")
 	encrypted_msg = encrypt(msg)
 	server.privmsg("cbot1", encrypted_msg)
-	client.process_once(.2)
 
+
+	
+#get_msg()
+runner = threading.Thread(target=lambda: client.process_forever())
+runner.start()
+
+#send_msg()
+# send_thread = threading.Thread(target=send_msg)
+# send_thread.start()
+
+while 1:
+	send_msg()
+	# time.sleep(1)
