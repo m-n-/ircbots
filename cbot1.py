a121891a6d465c737930589a64f73271e9007aa4
Michael <mpn123@gmail.com>
2014-07-14 15:03:46 +0200

    added AES encryption and it works properly

--- cbot1.py
+++ cbot1.py
@@ -2,13 +2,20 @@ import irc
 import irc.client
 import threading
 import time
+from Crypto.Cipher import AES
+import base64
 
 client = irc.client.IRC()
 server = client.server()
 server.connect("irc.freenode.net", 6667, "cbot1")
 
 
+#use AES encryption for 16 bit encryption
+obj=AES.new('1qazxsw23edcvfr4')
 
+
+#basic encryption add 4 and subtract 4 to message string
+'''
 def uncrypt(encrypted_string): #un-encrypt incoming messages
 	unencrypted_string = ""
 	for c in encrypted_string:
@@ -20,22 +27,36 @@ def encrypt(unencrypted_string):
 	for c in unencrypted_string:
 		encrypted_string += chr(ord(c) - 4)
 	return	encrypted_string
+'''
 
-def onrecieve(_, evt):
-	print(evt.source + ": " + uncrypt(" ".join(evt.arguments)))
+def onrecieve(_, evt): #process recieved message with decrypt and utf-decode >> strip white space with strip()
+	recieved_msg = base64.b64decode("".join(evt.arguments))
+	print(evt.source + ": " + obj.decrypt(recieved_msg).decode("utf-8").strip())
 
+#add handler to wait for recieved privmsg
 client.add_global_handler("privmsg", onrecieve)
 
 
-def send_msg():
-	# while 1:
+def send_msg(): #process sent message to make sure msg is 16 chars 
 	msg = input("")
-	encrypted_msg = encrypt(msg)
+
+	if (len(msg) < 16):
+		times = 16 - len(msg)
+		for i in range(1, times+1):
+			msg += " "
+
+	elif ((len(msg) % 16) != 0):
+		times = 16 - (len(msg) % 16)
+		for i in range(1, times+1):
+			msg += " "
+
+	#encrypt message and encode to b64 and utf-8		
+	encrypted_msg = base64.b64encode(obj.encrypt(msg)).decode("utf-8")
 	server.privmsg("cbot2", encrypted_msg)
 
 
 	
-#get_msg()
+#create thread to make sure the two processes run independently of each other so send and recieve can work in tandem
 runner = threading.Thread(target=lambda: client.process_forever())
 runner.start()
 
@@ -45,4 +66,3 @@ runner.start()
 
 while 1:
 	send_msg()
-	# time.sleep(1)
